ee2167279836c5fbdab0ed0d2a9d5ebd7c658668
wotsushi <wotsushi@gmail.com>
2020-07-09 22:15:12 +0900

    section 1.4

--- src.py
+++ src.py
@@ -2,6 +2,32 @@ from typing import Dict, Callable
 from math import sqrt
 
 
+def quad_util(a: float) -> Callable[[float], float]:
+    """
+    二次関数的な効用関数を返します。
+
+    Parameters
+    ----------
+    a: float
+        実係数
+
+    Returns
+    -------
+    Callable[[float], float]
+        以下を満たす効用関数u
+        - x <= a/2 について、 u(x) = ax^2 - x^2
+        - x > a/2 について、 u(x) = a^2 / 4
+    """
+
+    def u(x: float) -> float:
+        if x <= a / 2:
+            return a * x - x ** 2
+        else:
+            return a ** 2 / 4
+
+    return u
+
+
 def expectation(X: Dict[int, float]) -> float:
     """
     確率くじXの期待値を返します。
@@ -60,3 +86,54 @@ def expected_utility(u: Callable[[float], float], X: Dict[int, float]) -> float:
     """
 
     return sum(p * u(k) for k, p in X.items())
+
+
+def certainty_equivalent(u: Callable[[float], float], X: Dict[int, float]) -> float:
+    """
+    効用関数uの下で確率くじXの確実等価額を返します。
+
+    Parameters
+    ----------
+    u: Callable[[float], float]
+        上界が10^100, かつ、単調増加な効用関数
+    X: Dict[int, float]
+        確率くじ。X[k] = p とすると確率くじXは確率pでk円になることを表す。
+        Xのkeyをk_1, ..., k_n とするとき、X[k_1] + ... + X[k_n] = 1を満たす必要がある。
+
+    Returns
+    -------
+    float
+        uの下でのXの確実等価額
+    """
+
+    E_u = expected_utility(u, X)
+    ok = 1e100
+    ng = 0.0
+    while ok - ng > 1e-9:
+        mid = (ok + ng) / 2
+        if u(mid) >= E_u:
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+def risk_discount(u: Callable[[float], float], X: Dict[int, float]) -> float:
+    """
+    効用関数uの下で確率くじXのリスク・ディスカウント額を返します。
+
+    Parameters
+    ----------
+    u: Callable[[float], float]
+        上界が10^100, かつ、単調増加な効用関数
+    X: Dict[int, float]
+        確率くじ。X[k] = p とすると確率くじXは確率pでk円になることを表す。
+        Xのkeyをk_1, ..., k_n とするとき、X[k_1] + ... + X[k_n] = 1を満たす必要がある。
+
+    Returns
+    -------
+    float
+        uの下でのXのリスク・ディスカウント額
+    """
+
+    return expectation(X) - certainty_equivalent(u, X)
--- test_src.py
+++ test_src.py
@@ -1,8 +1,25 @@
 from typing import Dict, Callable
-from src import expectation, sd, expected_utility
+from src import (
+    quad_util,
+    expectation,
+    sd,
+    expected_utility,
+    certainty_equivalent,
+    risk_discount,
+)
 
 import pytest
 from pytest import approx
+
+
+@pytest.mark.parametrize(
+    "a,x,expected",
+    [(300, 0, 0), (300, 149, 22499), (300, 150, 22500), (300, 151, 22500)],
+)
+def test_quad_util(a: float, x: float, expected: float):
+    u = quad_util(a)
+    actual = u(x)
+    assert actual == approx(expected)
 
 
 @pytest.mark.parametrize(
@@ -34,13 +51,43 @@ def test_sd(X: Dict[int, float], expected: float):
 @pytest.mark.parametrize(
     "u,X,expected",
     [
-        (lambda x: 300 * x - x ** 2, {100: 0.5, 50: 0.5}, 16250),
-        (lambda x: 300 * x - x ** 2, {120: 0.5, 40: 0.5}, 16000),
-        (lambda x: 100 * x - x ** 2, {40: 0.4, 30: 0.3, 20: 0.2, 10: 0.1}, 2000),
+        (quad_util(300), {100: 0.5, 50: 0.5}, 16250),
+        (quad_util(300), {120: 0.5, 40: 0.5}, 16000),
+        (quad_util(100), {40: 0.4, 30: 0.3, 20: 0.2, 10: 0.1}, 2000),
     ],
 )
 def test_expected_utility(
     u: Callable[[float], float], X: Dict[int, float], expected: float
 ):
     actual = expected_utility(u, X)
+    assert actual == approx(expected)
+
+
+@pytest.mark.parametrize(
+    "u,X,expected",
+    [
+        (quad_util(300), {100: 0.5, 50: 0.5}, 70.94305849579051),
+        (quad_util(300), {120: 0.5, 40: 0.5}, 69.37742251701451),
+        (quad_util(100), {40: 0.4, 30: 0.3, 20: 0.2, 10: 0.1}, 27.639320225002102),
+    ],
+)
+def test_certainty_equivalent(
+    u: Callable[[float], float], X: Dict[int, float], expected: float
+):
+    actual = certainty_equivalent(u, X)
+    assert actual == approx(expected)
+
+
+@pytest.mark.parametrize(
+    "u,X,expected",
+    [
+        (quad_util(300), {100: 0.5, 50: 0.5}, 4.05694150420949),
+        (quad_util(300), {120: 0.5, 40: 0.5}, 10.622577482985491),
+        (quad_util(100), {40: 0.4, 30: 0.3, 20: 0.2, 10: 0.1}, 2.360679774997898),
+    ],
+)
+def test_risk_discount(
+    u: Callable[[float], float], X: Dict[int, float], expected: float
+):
+    actual = risk_discount(u, X)
     assert actual == approx(expected)
